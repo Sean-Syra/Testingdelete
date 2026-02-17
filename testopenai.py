e0b6a4d1ade097e54e43fd5e2dc468f8f70790e0
Sean-Syra <140710004+Sean-Syra@users.noreply.github.com>
2026-02-17 00:41:02 -0500

    initial commit

---
+++ testopenai.py
@@ -0,0 +1,13 @@
+import os
+from openai import OpenAI
+
+
+api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key=api_key)
+
+response = client.responses.create(
+    model="gpt-5.2",
+    input="Write a one-sentence bedtime story about a unicorn."
+)
+
+print(response.output_text)
